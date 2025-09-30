798024643a71225cbf5f1b6c407971ad6660d6c2
Chloe Chung <chloechung@Chloes-MacBook-Air-9.local>
2025-09-30 01:29:03 -0700

    remove projectbot.py

--- projectbot.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-opts = Options()
-opts.add_experimental_option('detach', True)
-driver = webdriver.Chrome(options=opts)
-driver.get("https://cmsweb.fullerton.edu/psc/CFULPRD/EMPLOYEE/SA/c/SA_LEARNER_SERVICES.CLASS_SEARCH.GBL?&public")
