ef784a03fb405eec09023d1c8e1b8dbe4773d1ff
Eugene Kim <eugene@Eugenes-MacBook-Pro.local>
2025-10-21 17:51:44 -0700

    Delete Course_id router and function

--- Backend/TitanApi/app/routers/courses_router.py
+++ Backend/TitanApi/app/routers/courses_router.py
@@ -29,6 +29,23 @@ def get_courses(course_id: str):
         raise HTTPException(status_code=404, detail="Course not found!")
     return c
 
-@router.post("/courses")
+@router.get("/courses")
 def get_all_courses():
     return courses_crud.get_all_courses()
+
+@router.delete("/courses/{course_id}")
+def delete_courses(course_id):
+    # empty value is accepted
+    if course_id is None:
+        raise HTTPException(
+            status_code=400,
+            detail="Please Enter Course_ID"
+        )
+    ok = courses_crud.delete_courses(course_id)
+    
+    if not ok: # Course_id NOT FOUNd
+        raise HTTPException(
+            status_code=404,
+            detail="course_id Not Found"
+        )
+    return {f"Successfully Deleted {course_id}!"}
