3a70a5395c9d9e7e6cdd5496e5042e9d6dc8af08
Kimchyboy <eugene2819@hotmail.com>
2025-10-21 19:52:08 -0700

    Added Middleware for Frontend

--- Backend/TitanApi/app/main.py
+++ Backend/TitanApi/app/main.py
@@ -1,10 +1,21 @@
 import sqlite3
 from fastapi import FastAPI, APIRouter, HTTPException
 from routers import terms_router, courses_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(terms_router.router)
 app.include_router(courses_router.router)
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or specify your frontend domain
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get('/')
 def root():
