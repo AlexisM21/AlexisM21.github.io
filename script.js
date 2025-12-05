5627431bd7521e51bafa4b87b8dfc0b057f516ea
unknown <chloechung53@gmail.com>
2025-12-04 17:52:22 -0800

    designing

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 /* script.js */
+
+function showPage(pageId) {
+  const pages = document.querySelectorAll(".page");
+  pages.forEach(page => {
+    page.classList.toggle("active", page.id === pageId);
+  });
+}
+
+document.querySelectorAll(".next-button").forEach(button => {
+  button.addEventListener("click", () => {
+    showPage(button.getAttribute("data-next"));
+  });
+});
+
+document.querySelectorAll(".back-button").forEach(button => {
+  button.addEventListener("click", () => {
+    showPage(button.getAttribute("data-prev"));
+  });
+});
